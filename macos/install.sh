572a2a2c9baac9e1350566096b671ef8df96d413
phatpham9 <phatpham9@gmail.com>
2019-11-22 15:42:01 +0700

    feat(macos): comment out enable Airdrop over Ethernet

--- macos/install.sh
+++ macos/install.sh
@@ -349,7 +349,7 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
 # Enable AirDrop over Ethernet and on unsupported Macs running Lion
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+# defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 
 # Show the ~/Library folder
 # chflags nohidden ~/Library
