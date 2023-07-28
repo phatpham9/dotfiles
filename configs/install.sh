39fb6f827eaccb1f44ccf340a3d182be3f659aa8
Phat Pham <phat@onroads.xyz>
2023-07-29 00:48:28 +0700

    feat(configs): add git gpg ssh secondary config

--- configs/install.sh
+++ configs/install.sh
@@ -9,6 +9,8 @@ echo "-> zsh configured!"
 # gpg
 gpg --batch --generate-key "gpg/generate-key"
 gpg --armor --export phat@onroads.xyz
+gpg --batch --generate-key "gpg/generate-key_secondary"
+gpg --armor --export phatttpham9@gmail.com
 gpg --batch --generate-key "gpg/generate-key_setel"
 gpg --armor --export phat.pham@setel.com
 echo "-> gpg key generated!"
@@ -16,18 +18,23 @@ echo "-> Attention! Don't forget to copy the public key & add it to remote hosts
 
 # ssh
 ssh-keygen -t ed25519 -C "phat@onroads.xyz" -f "${HOME}/.ssh/id_ed25519" -P ""
+ssh-keygen -t ed25519 -C "phatttpham9@gmail.com" -f "${HOME}/.ssh/id_ed25519_secondary" -P ""
 ssh-keygen -t ed25519 -C "phat.pham@setel.com" -f "${HOME}/.ssh/id_ed25519_setel" -P ""
 ln -sf ${DIR}/ssh/config ~/.ssh/config
+ln -sf ${DIR}/ssh/config_secondary ~/.ssh/config_secondary
+ln -sf ${DIR}/ssh/config_setel ~/.ssh/config_setel
 echo "-> ssh key generated & configured!"
 echo "-> Attention! Don't forget to copy the public key & add it to remote hosts such as github.com & gitlab.com."
 
 # git
 ln -sf ${DIR}/git/gitconfig ~/.gitconfig
+ln -sf ${DIR}/git/gitconfig_secondary ~/.gitconfig_secondary
 ln -sf ${DIR}/git/gitconfig_setel ~/.gitconfig_setel
 ln -sf ${DIR}/git/gitignore_global ~/.gitignore_global
-keys=($(gpg --list-keys --keyid-format LONG | grep -E -o -m 2 "ed25519/[0-9A-F]{16}" | grep -E -o -m 2 "[0-9A-F]{16}"))
+keys=($(gpg --list-keys --keyid-format LONG | grep -E -o -m 3 "ed25519/[0-9A-F]{16}" | grep -E -o -m 3 "[0-9A-F]{16}"))
 git config --file ~/.gitconfig user.signingkey $keys[1]
-git config --file ~/.gitconfig_setel user.signingkey $keys[2]
+git config --file ~/.gitconfig_secondary user.signingkey $keys[2]
+git config --file ~/.gitconfig_setel user.signingkey $keys[3]
 echo "-> git configured!"
 echo "-> Attention! Git config \"user.signingkey\" has been updated. Don't forget to commit & push your changes to remote repository."
 
