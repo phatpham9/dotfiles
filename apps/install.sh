6758a14bdfc7e70d16f3a5be685a5729053ea754
Phat Pham <phat@onroads.xyz>
2023-11-23 13:24:31 +0700

    feat(apps): move macos Brewfile to different file

--- apps/install.sh
+++ apps/install.sh
@@ -7,22 +7,26 @@ is_macos() {
   [[ $(uname) == "Darwin" ]]
 }
 
-
-# xcode
+# pre-install
 if is_macos; then
+  # xcode
   xcode-select --install
   echo "-> xcode installed!"
+else
+  sudo apt update
+  sudo apt install build-essential procps curl file git
 fi
 
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "-> brew installed!"
+eval "$($(brew --prefix)/bin/brew shellenv)"
 
 # cli & gui apps by brew
-brew bundle --file=${DIR}/cli
 if is_macos; then
-  brew bundle --file=${DIR}/gui
+  brew bundle --file=${DIR}/Brewfile_macos
   echo "-> brew cli & gui apps installed!"
 else
+  brew bundle --file=${DIR}/Brewfile
   echo "-> brew cli apps installed!"
 fi
