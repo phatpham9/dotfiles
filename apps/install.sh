0944a3c46fb1cc355a9c2ca9b826bad8a8fd570f
Phat Pham <phat@onroads.xyz>
2023-11-23 12:23:56 +0700

    feat(apps): separate cli & gui into different files for different os

--- apps/install.sh
+++ apps/install.sh
@@ -2,14 +2,27 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# check if running on macOS
+is_macos() {
+  [[ $(uname) == "Darwin" ]]
+}
+
+
 # xcode
-xcode-select --install
-echo "-> xcode installed!"
+if is_macos; then
+  xcode-select --install
+  echo "-> xcode installed!"
+fi
 
 # brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "-> brew installed!"
 
 # cli & gui apps by brew
-brew bundle --file=${DIR}/
-echo "-> brew cli & gui apps installed!"
+brew bundle --file=${DIR}/cli
+if is_macos; then
+  brew bundle --file=${DIR}/gui
+  echo "-> brew cli & gui apps installed!"
+else
+  echo "-> brew cli apps installed!"
+fi
