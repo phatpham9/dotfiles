1485d7dfcc847e221537e54a9e1e82e33eabbd74
Phat Pham <phat@onroads.xyz>
2023-11-27 01:09:49 +0700

    feat(apps/install.sh): don't run zsh after installed, make homebrew install noninteractive

--- apps/install.sh
+++ apps/install.sh
@@ -13,23 +13,17 @@ if is_macos; then
   echo "-> xcode installed!"
 else
   sudo apt-get update
-  sudo apt-get install gcc build-essential procps file curl git zsh
-  chsh -s $(which zsh)  # change the default shell to zsh. prompt for password
-  echo "-> build tools installed!"
-fi
-
-# install oh-my-zsh
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  echo "-> oh-my-zsh installed!"
+  sudo apt-get install build-essential procps file curl git zsh
+  echo "-> curl git zsh installed!"
 fi
 
 # install brew
 if ! command -v brew &> /dev/null; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo "-> brew installed!"
   if is_macos; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
+    brew install gcc
   else
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   fi
@@ -38,8 +32,14 @@ fi
 # install cli & gui apps
 brew bundle --file=${DIR}/Brewfile_cli
 if is_macos; then
-  brew bundle --file=${DIR}/Brewfile_macos
+  brew bundle --file=${DIR}/Brewfile_gui
   echo "-> cli & gui apps installed!"
 else
   echo "-> cli apps installed!"
+fi
+
+# install oh-my-zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  RUNZSH=no /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo "-> oh-my-zsh installed!"
 fi
