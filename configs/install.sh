c4bfd530caaf7e3f9108466dd00229e1f9504772
Phat Pham <phat@onroads.xyz>
2023-12-01 00:32:57 +0700

    feat(apps, configs): add gh direnv clis, gh fnm direnv thefuck omz plugins

--- configs/install.sh
+++ configs/install.sh
@@ -2,10 +2,6 @@
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# configure zsh
-ln -sf ${DIR}/zsh/zshrc ${HOME}/.zshrc
-echo "-> zsh configured!"
-
 # configure ssh
 ssh-keygen -t ed25519 -C "phat@onroads.xyz" -f "${HOME}/.ssh/id_ed25519" -P ""
 ssh-keygen -t ed25519 -C "phatttpham9@gmail.com" -f "${HOME}/.ssh/id_ed25519_secondary" -P ""
@@ -22,3 +18,16 @@ ln -sf ${DIR}/git/gitconfig_secondary ${HOME}/.gitconfig_secondary
 ln -sf ${DIR}/git/gitconfig_setel ${HOME}/.gitconfig_setel
 ln -sf ${DIR}/git/gitignore_global ${HOME}/.gitignore_global
 echo "-> git configured!"
+
+# configure gh
+ln -sf ${DIR}/gh/config.yml ${HOME}/.config/gh/config.yml
+ln -sf ${DIR}/gh/hosts.yml ${HOME}/.config/gh/hosts.yml
+ln -sf ${DIR}/gh/config.yml ${HOME}/.config/gh_secondary/config.yml
+ln -sf ${DIR}/gh/hosts.yml_secondary ${HOME}/.config/gh_secondary/hosts.yml
+ln -sf ${DIR}/gh/config.yml ${HOME}/.config/gh_setel/config.yml
+ln -sf ${DIR}/gh/hosts.yml_setel ${HOME}/.config/gh_setel/hosts.yml
+echo "-> gh configured!"
+
+# configure zsh
+ln -sf ${DIR}/zsh/zshrc ${HOME}/.zshrc
+echo "-> zsh configured!"
