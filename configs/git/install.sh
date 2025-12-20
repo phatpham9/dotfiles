60dd66552b8b7f633fdff0106bf23499ca029dbc
Phat Pham <phat@onroads.xyz>
2025-12-20 15:09:09 +0700

    fix(configs/git): add missing git-lfs setup command

--- configs/git/install.sh
+++ configs/git/install.sh
@@ -9,6 +9,9 @@ source "${DIR}/../../utils/create_symlink.sh"
 configure_git() {
   create_symlink "${DIR}/gitconfig" "${HOME}/.gitconfig"
   create_symlink "${DIR}/gitignore_global" "${HOME}/.gitignore_global"
+  
+  # Initialize git-lfs hooks
+  git lfs install
 }
 
 configure_git
